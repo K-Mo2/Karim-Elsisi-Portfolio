64deaaa48c2394bd771cbad65d1b579560e5458c
K-Mo2 <karimelsisi27@gmail.com>
2021-12-03 20:26:58 +0200

    getting started with the layout

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 import "./testimonials.scss";
 export default function Testimonials() {
-  return <div></div>;
+  return <div className="testimonials" id="testimonials"></div>;
 }
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,5 +1,16 @@
 import React from "react";
-import "./topbar.scss"
+import "./topbar.scss";
 export default function Topbar() {
-  return <div></div>;
+  return (
+    <div className="topbar" id="topbar">
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            K_Mo.
+          </a>
+        </div>
+        <div className="right">I'm right</div>
+      </div>
+    </div>
+  );
 }
