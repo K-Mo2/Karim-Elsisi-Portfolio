23764682332d66f6960cb42dc023a0d0b32356e2
K-Mo2 <karimelsisi27@gmail.com>
2021-12-03 20:56:03 +0200

    adding contact icons and contact info on the topbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./topbar.scss";
+import { Person, Mail } from "@material-ui/icons";
+
 export default function Topbar() {
   return (
     <div className="topbar" id="topbar">
@@ -8,6 +10,15 @@ export default function Topbar() {
           <a href="#intro" className="logo">
             K_Mo.
           </a>
+          <div className="itemContainer">
+            {" "}
+            <Person className="icon" />
+            <span>+20 1146188602</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>karimelsisi27@gmail.com</span>
+          </div>
         </div>
         <div className="right">I'm right</div>
       </div>
