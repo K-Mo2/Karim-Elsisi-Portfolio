0692d3aa5a125d28bafca109a6019e051716af97
K-Mo2 <karimelsisi27@gmail.com>
2021-12-04 22:50:15 +0200

    adding animated hamburger menu

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import "./topbar.scss";
 import { Person, Mail } from "@material-ui/icons";
 
-export default function Topbar() {
+export default function Topbar({ menuOpen, setMenuOpen }) {
   return (
-    <div className="topbar" id="topbar">
+    <div className={"topbar " + (menuOpen && "active")} id="topbar">
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">
@@ -20,7 +20,13 @@ export default function Topbar() {
             <span>karimelsisi27@gmail.com</span>
           </div>
         </div>
-        <div className="right">I'm right</div>
+        <div className="right">
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
       </div>
     </div>
   );
