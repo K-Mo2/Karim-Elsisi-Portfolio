445134ae5c13ae8de0c287acb9c5636b5d469562
K-Mo2 <karimelsisi27@gmail.com>
2021-12-05 01:46:40 +0200

    adding typing effect ot the intro section

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 import "./intro.scss";
+import { init } from "ityped";
+import { useRef, useEffect } from "react";
 
 export default function Intro() {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ["Programmer.", "Developer."],
+    });
+  }, []);
   return (
     <div className="intro" id="intro">
       <div className="left">
@@ -14,7 +26,7 @@ export default function Intro() {
           <h2>Hi there, I'm</h2>
           <h1>Karim Elsisi</h1>
           <h3>
-            I'm a<span></span>
+            I'm a <span ref={textRef}></span>
           </h3>
         </div>
         <a href="#portfolio">
