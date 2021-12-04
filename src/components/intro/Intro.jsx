42b9361b83d99e15791ed5a42cbc711232deecba
K-Mo2 <karimelsisi27@gmail.com>
2021-12-05 01:24:47 +0200

    adding the intro section

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,5 +1,26 @@
 import React from "react";
 import "./intro.scss";
+
 export default function Intro() {
-  return <div className="intro" id="intro"></div>;
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imageContainer">
+          <img src="assets/man.png" alt="man" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi there, I'm</h2>
+          <h1>Karim Elsisi</h1>
+          <h3>
+            I'm a<span></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="down arrow" />
+        </a>
+      </div>
+    </div>
+  );
 }
