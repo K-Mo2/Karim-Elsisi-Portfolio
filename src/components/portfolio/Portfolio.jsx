46054520c9f3d2938e1648a5d8f9b35e6c863758
K-Mo2 <karimelsisi27@gmail.com>
2021-12-05 11:43:59 +0200

    adding the portfolio section with tabs

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,50 @@
-import React from "react";
+import {useState} from "react";
 import "./portfolio.scss";
+import PortfolioList from "../portfolioList/PortfolioList";
+
 export default function Portfolio() {
-  return <div className="portfolio" id="portfolio"></div>;
+  const [selected, setSelected] = useState('featured');
+  const list = [
+    { id: "featured", title: "Featured" },
+    { id: "web", title: "Web App" },
+    { id: "mobile", title: "Mobile App" },
+    { id: "design", title: "Design" },
+    { id: "content", title: "Content" },
+  ];
+  return (
+    <div className="portfolio" id="portfolio">
+      <h1>Portfolio</h1>
+      <ul>
+        {list.map((item) => (
+          <PortfolioList title={item.title} active={selected === item.id} setSelected={setSelected} id={item.id}/>
+        ))}
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img src="assets/globe.png" alt="man" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="assets/globe.png" alt="man" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="assets/globe.png" alt="man" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="assets/globe.png" alt="man" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="assets/globe.png" alt="man" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="assets/globe.png" alt="man" />
+          <h3>Banking App</h3>
+        </div>
+      </div>
+    </div>
+  );
 }
